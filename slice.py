221cba531eaa5b202a5068f8d77bd39d146cb4a7
Abhi9893 <patsariya.abhi@gmail.com>
2024-08-09 18:55:12 +0530

    jngjingjibng

---
+++ slice.py
@@ -0,0 +1,16 @@
+name = "pn infosys gwalior" #index 0,1,2,3
+# print(name[1]) #
+#name[0] = "abhi" #not change string
+#print(name[0:4]) #start 0 end 5
+#print(name)
+#print(name[:4]) # is same as name [0:4]
+#print(name[1:4])
+#print(name[1:]) #is name as name[1:5]
+print(name[-4:-1])
+#print(name)
+
+# #string are arrays
+
+name ="prinf"
+c =name[2::1]
+print(c)
