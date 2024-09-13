5156e63671fb5d071d6531a411b5d12f40b1fe19
Abhi9893 <patsariya.abhi@gmail.com>
2024-09-13 19:00:30 +0530

    hiiiii

---
+++ condition/nested_if.py
@@ -0,0 +1,11 @@
+x = 18
+
+if x > 20:        # =use
+    print("Above 15,")
+    if x < 2:
+        print("and also above 25!")
+    else:
+        print("but not above 20.")
+else:
+    print("no")
+    
---
+++ program/program9.py
@@ -0,0 +1,11 @@
+sub1 = int(input("Enter first subject marks\n "))
+sub2 = int(input("Enter second subject marks\n "))
+sub3 = int(input("Enter third subject marks\n "))
+
+if(sub1<33 or sub<33 or sub<33):
+    print("you are fail because you have less then 33% in one subject")
+elif(sub1+sub2+sub3)/3 < 40:
+    print("you are fail due to total percentage less than 40")
+else:
+    print("congatulations! you passes the exam")
+
