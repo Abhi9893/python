cfe5916ba18d6cd9c792c8cff6adeea67642c242
Abhi9893 <patsariya.abhi@gmail.com>
2024-09-25 18:54:47 +0530

    hiii

--- loop/forloop.py
+++ loop/forloop.py
@@ -1,5 +1,28 @@
-course = ["html","python","laravel","css","javascript"]
+# course = ["html","python","laravel","css","javascript"]
 
-for a in course:
+# for b in course:
 
- print(a)
+#  print(b)
+
+#  # #course
+#  course = ["html","paython","laravel","css","javascript"]
+#  #print(len(course))
+#  i=0
+#  while i<len(course):
+#    print(course[i])  #html
+#    i = i+1
+
+# course = ["html","paython","laravel","css","javascript"]
+# for a in course:
+#   print(a)
+
+students = [
+  ["ram",34,"gwalior","india"],
+  ["rohitm",34,"gwalior","india"],
+  ["raj",34,"gwalior","india"],
+  ["viki",34,"gwalior","india"]
+]
+
+# print(students)
+for b in students:
+  print(b)
